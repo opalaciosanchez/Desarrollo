cd850cd623277fec0c18f9deb393331a3b5907b7
opalaciosanchez <oscar.palacios@outlook.com>
2015-05-06 10:27:41 +0200

    solucionada la conexión y carga de items

--- modelos-pago/php/class.Functions.php
+++ modelos-pago/php/class.Functions.php
@@ -4,7 +4,7 @@ class Functions {
 	private static $_conexionEstablecida;
 	// creamos la conexión y almacenamos su valor para ser llamado cuando sea preciso
 	private static function _establecerConexion() {
-		$db = Database::getInstancia();
+		$db                   = Database::getInstancia();
 		$_conexionEstablecida = $db->getConexion();
 		return $_conexionEstablecida;
 	}
@@ -15,8 +15,8 @@ class Functions {
 			unset(self::$_conexionEstablecida);
 		}
 	}
-	
-	// la primera de los métodos de "uso" comprueba si la consulta realizada es correcta. 
+
+	// la primera de los métodos de "uso" comprueba si la consulta realizada es correcta.
 	// la función usa un argumento que será pasado en el momento de su uso
 	// la hacemos publica para que pueda ser usada, y al estar en el interior de la clase puede hacer uso del método privado anterior
 	public static function confirma_consulta($result_query) {
@@ -28,18 +28,17 @@ class Functions {
 			die("la consulta ha fallado por: ".mysqli_error($mysqli));
 		}
 	}
-	
+
 	// metodo que ejecuta las consultas de seleccion genericas para evitar codigo repetido. Después podremos seleccionar los campos en la salida
 	public static function consulta($tabla) {
 		$mysqli = self::_establecerConexion();
-		$query = "SELECT *
+		$query  = "SELECT *
 				FROM {$tabla}
-				ORDER BY producto ASC";		
+				ORDER BY nombre ASC";
 		$result_set = mysqli_query($mysqli, $query);
 		self::confirma_consulta($result_set);
 		return $result_set;
 	}
-	
 
 	// llamada a todos los productos
 	public static function listado($tabla) {
@@ -50,7 +49,7 @@ class Functions {
 		$output = self::salida($te_set);
 		return $output;
 	}
-	
+
 	// metodo que utiliza los resultados de la consulta y los muestra por pantalla
 	public static function salida($tipo) {
 		$output = "";
@@ -61,7 +60,7 @@ class Functions {
 		}
 		return $output;
 	}
-	
+
 	/* ESTA SECCIÓN CONTROLA EL FILTRADO DE LOS PRODUCTOS SEGÚN EL FORMULARIO DE ESA MISMA PÁG */
 	public static function filtrado_tipo($producto, $precio_menor, $precio_mayor) {
 		$mysqli = self::_establecerConexion();
@@ -69,12 +68,12 @@ class Functions {
 		if ($precio_menor == 0 && $precio_mayor == 0) {
 			// llamamos al método que ejecuta las consultas genéricas y devuleve resultados
 			$result_set = self::consulta($producto);
-			
-				$output = self::salida($result_set);
-				return $output;
+
+			$output = self::salida($result_set);
+			return $output;
 		}
-		// si se detecta un valor de filtrado de precio... 
-		else {
+		// si se detecta un valor de filtrado de precio...
+		 else {
 			$query = "SELECT *";
 			$query .= "FROM {$producto} WHERE precio BETWEEN {$precio_menor} AND {$precio_mayor}";
 			$result_set = mysqli_query($mysqli, $query);
@@ -88,34 +87,34 @@ class Functions {
 			}
 		}
 	}
-	
+
 	/* CREACIÓN DE NUEVOS PRODUCTOS */
 	// creamos primero un método protegido (para evitar creaciones de productos no permitidas) que genera los productos
 	private static function insertarProducto($tabla, $columnas, $datos) {
-	// capturamos los valores pasados como matriz para campo y valor	
+		// capturamos los valores pasados como matriz para campo y valor
 		$campo1 = $columnas[0];
 		$campo2 = $columnas[1];
 		$campo3 = $columnas[2];
 		$campo4 = $columnas[3];
-		
+
 		$valor1 = $datos[0];
 		$valor2 = $datos[1];
 		$valor3 = $datos[2];
 		$valor4 = $datos[3];
-		
- 		$mysqli = self::_establecerConexion();
-		$query = "INSERT INTO {$tabla} ({$campo1}, {$campo2}, {$campo3}, {$campo4})
+
+		$mysqli = self::_establecerConexion();
+		$query  = "INSERT INTO {$tabla} ({$campo1}, {$campo2}, {$campo3}, {$campo4})
 				 VALUES ('{$valor1}', '{$valor2}', '{$valor3}', '{$valor4}')";
 		$insertar = mysqli_query($mysqli, $query);
 		return $insertar;
 	}
-	
+
 	// evaluamos qué tipo de producto ha de insertarse en la base de datos
 	public static function nuevo_producto($tipo, $nombre, $precio, $descripcion, $nombre_img) {
 		$mysqli = self::_establecerConexion();
-		
+
 		// creamos las matrices que almacenarán los valores
-		$campos = array('nombre', 'precio', 'descripcion', 'imagen');
+		$campos  = array('nombre', 'precio', 'descripcion', 'imagen');
 		$valores = array($nombre, $precio, $descripcion, $nombre_img);
 
 		switch ($tipo) {
@@ -139,14 +138,13 @@ class Functions {
 				break;
 		}
 	}
-	
-	
+
 	/* COMPROBACIÓN DE USUARIOS */
 	// creamos la función que recuperará el listado de usuario y contraseña
 	public static function comprueba_usuario($usuario) {
 		$mysqli = self::_establecerConexion();
 		// sólo devolvemos el usuario cuando coincida el nombre
-		$query = "SELECT usuario,password FROM usuarios WHERE usuario = '{$usuario}'";
+		$query      = "SELECT usuario,password FROM usuarios WHERE usuario = '{$usuario}'";
 		$result_set = mysqli_query($mysqli, $query);
 		return $result_set;
 	}
