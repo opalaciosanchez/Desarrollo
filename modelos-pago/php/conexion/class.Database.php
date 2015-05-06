7c9c32d70619b696309ac6484b1bde3a89daeda6
opalaciosanchez <oscar.palacios@outlook.com>
2015-05-06 09:38:00 +0200

    posible error al definir el servidor localhost en MySQL

--- modelos-pago/php/conexion/class.Database.php
+++ modelos-pago/php/conexion/class.Database.php
@@ -15,9 +15,10 @@ class Database {
 		// comprobamos si hay una instancia ya creada o no
 		// si no es así, la generamos automáticamente. Usamos self:: porque es una propiedad estática
 		if (!self::$_instancia) {
-		// fijemonos en que estamos almacenando UNA NUEVA INSTANCIA DE LA CLASE (new self) DENTRO de la variable instancia
-		// estamos añadiendo un paso intermedio al instanciamiento directo de la clase en el código posterior
-			self::$_instancia = new self(); // sería lo mismo que poner new Database();
+			// fijemonos en que estamos almacenando UNA NUEVA INSTANCIA DE LA CLASE (new self) DENTRO de la variable instancia
+			// estamos añadiendo un paso intermedio al instanciamiento directo de la clase en el código posterior
+			self::$_instancia = new self();
+			// sería lo mismo que poner new Database();
 		}
 		// si existe la instancia (tiene valor) la devolvemos para poder usarla en la conexión
 		return self::$_instancia;
@@ -29,15 +30,14 @@ class Database {
 		// instanciamos la clase mysqli en nuestra propia conexion, insertando los valores de la misma
 		$this->_conexion = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 		// comprobamos que la conexión se ha establecido
-		if(mysqli_connect_error()) {
-			echo "Ha habido un error al conectar con la DB MySQL: " . mysqli_connect_error();
+		if (mysqli_connect_error()) {
+			echo "Ha habido un error al conectar con la DB MySQL: ".mysqli_connect_error();
 		}
 		// seleccionamos la base de datos a usar
-	
- 		$this->_seleccionDB = mysqli_select_db($_conexion, DB_NAME);
-			if (!$_seleccionDB) {
-				die("la base de datos seleccionada no esta disponible");
-		}
+		// 	$this->_seleccionDB = mysqli_select_db($_conexion, DB_NAME);
+		// 	if (!$_seleccionDB) {
+		// 		die("la base de datos seleccionada no esta disponible");
+		// }
 
 		// nos aseguramos de que la conexión entre la DB y la página acepte ñ y tildes
 		mysqli_set_charset($this->_conexion, "utf8");
@@ -49,6 +49,6 @@ class Database {
 	public function getConexion() {
 		return $this->_conexion;
 	}
-	
+
 }
 ?>
